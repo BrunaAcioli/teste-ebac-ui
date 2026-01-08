27dafa807c098a747f18450da9f9f3930f75cd07
Bruna Acioli <brunaacioli09@gmail.com>
2026-01-08 10:43:47 -0300

    Login atual

--- cypress/e2e/3-loja-ebac/login atual.cy.js
+++ cypress/e2e/3-loja-ebac/login atual.cy.js
@@ -1,39 +1,61 @@
 ///<reference types="cypress"/>
 
-describe( 'Funcionalidade: Login', () =>{
+const perfil = require("../../fixtures/perfil.json")
 
- beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
- });   
+describe('Funcionalidade: Login', () => {
 
- afterEach(() => {
+  beforeEach(() => {
+    cy.visit('/minha-conta')
+  })
+
+  afterEach(() => {
     cy.screenshot()
- });
- 
- it('Deve fazer login com sucesso', () =>{
+  })
+
+  it('Deve fazer login com sucesso', () => {
     cy.get('#username').type('brunaacioli09@gmail.com')
     cy.get('#password').type('Bj180719*')
     cy.get('.woocommerce-form > .button').click()
 
-    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, brunaacioli09 (não é brunaacioli09? Sair)') 
+    cy.get('.woocommerce-MyAccount-content')
+      .should('contain', 'Olá, brunaacioli09')
+  })
 
- } )   
-
- it('Deve exibir uma mensagem de erro ao  inserir usuário inválido', () => {
+  it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
     cy.get('#username').type('trem.amor@gmail.com')
     cy.get('#password').type('Bj180719*')
     cy.get('.woocommerce-form > .button').click()
 
-    cy.get('.woocommerce-error'). should('exist')
- });
-    
- it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+    cy.get('.woocommerce-error').should('exist')
+  })
+
+  it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
     cy.get('#username').type('brunaacioli09@gmail.com')
     cy.get('#password').type('mj050409-')
     cy.get('.woocommerce-form > .button').click()
 
-    cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail brunaacioli09@gmail.com está incorreta. Perdeu a senha?')
-    cy.get('.woocommerce-error'). should('exist')
-    
- });
+    cy.get('.woocommerce-error')
+      .should('contain', 'senha fornecida')
+  })
+
+  it('Deve fazer login com sucesso - Usando massa de dados', () => {
+    cy.get('#username').type(perfil.usuário)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-MyAccount-content')
+      .should('contain', 'Olá, brunaacioli09')
+  })
+
+  it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+    cy.fixture('perfil').then(dados => {
+      cy.get('#username').type(dados.usuário , {log: false})
+      cy.get('#password').type(dados.senha , {log: false})
+      cy.get('.woocommerce-form > .button').click()
+
+      cy.get('.woocommerce-MyAccount-content')
+        .should('contain', 'Olá, brunaacioli09')
+    })
+  })
+
 })
